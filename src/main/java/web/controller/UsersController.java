179074d24bea0e410e79fe3d44872c7ebaeb042f
Viktor Aleynikov <A96VA@yandex.ru>
2022-11-29 12:58:38 +0500

    - HelloController - void

--- src/main/java/web/controller/UsersController.java
+++ src/main/java/web/controller/UsersController.java
@@ -18,69 +18,48 @@ public class UsersController {
         this.userService = userService;
     }
 
-    @GetMapping()   //выводит 10 user или заданное кол-во
+    @GetMapping()
     public String countUsers(@RequestParam(name = "count", defaultValue = "10") int count, Model model) {
         model.addAttribute("listUsers", userService.countUsers(count));
         return "users";
     }
 
-    @GetMapping("/{id}")  //выводит юзера по Id
+    @GetMapping("/{id}")
     public String show(@PathVariable("id") int id, Model model) {
         model.addAttribute("user", userService.show(id));
         return "show";
     }
 
-    @GetMapping("/new") //создание нового юзера
+    @GetMapping("/new")
     public String newUser(Model model) {
         model.addAttribute("user", new User());
         return "new";
     }
 
-    @PostMapping()//создание нового юзера
+    @PostMapping()
     public String create(@ModelAttribute("user") User user) {
         userService.save(user);
         return "redirect:/users";
     }
 
-    @GetMapping("/{id}/edit") //обновление юзера
+    @GetMapping("/{id}/edit")
     public String edit(Model model, @PathVariable("id") int id) {
         model.addAttribute("user", userService.show(id));
         return "edit";
     }
 
-    //    {id}(id=${user.getId()}) {nomer}(nomer=${user.getNomer()}) {nameUs}(nameUs=${user.getNameUs()}) {lastNameUs}(lastNameUs=${user.getLastNameUs()}
-    @PatchMapping("/{id}") //обновление юзера
+
+    @PatchMapping("/{id}")
     public String update(@PathVariable("id") int id, @ModelAttribute("user") User user) {
         userService.update(id, user);
         return "redirect:/users";
     }
 
-    @DeleteMapping("/{id}")//удаление юзера
+    @DeleteMapping("/{id}")
     public String delete(@PathVariable("id") int id) {
         userService.delete(id);
         return "redirect:/users";
     }
 }
-
-//    {nomer}{nameUs}{lastNameUs}
-//    @PathVariable("nomer") int nomer,
-//    @PathVariable("nameUs") String nameUs,
-//    @PathVariable("lastNameUs") String lastNameUs
-
-
-//}
-//    @PostMapping()
-//    public String create (@RequestParam("gosNomer") String gosNomer,
-//                          @RequestParam("brand") String brand,
-//                          @RequestParam("color") String color){
-//        Car car = new Car();
-//        car.setGosNomer(gosNomer);
-//        car.setBrand(brand);
-//        car.setColor(color);
-//        carService.save(car);
-//
-//        return "redirect:/cars";
-//    }
-
 
 
